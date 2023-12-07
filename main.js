7cf0d746ea0d26704629df846513d5b7ab41f70c
Franco Giovanniello <f.giovanniello98@gmail.com>
2023-12-06 21:36:45 -0300

    Entregable dos, Javascript

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 // Array de productos
 const products = [
+  {
+    "id": 3,
+    "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    "price": 700,
+    "description": "De nuestra colección Legends, el Naga se inspiró en el mítico dragón de agua que protege la perla del océano. Úselo mirando hacia adentro para recibir amor y abundancia, o hacia afuera para protección..",
+    "category": "Joyeria",
+    "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    "rating": {
+    "rate": 4.6,
+    "count": 400
+    }
+    },
     {
         "id": 1,
         "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-        "price": 109.95,
-        "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-        "category": "men's clothing",
+        "price": 109,
+        "description": "Tu mochila perfecta para el uso diario y los paseos por el bosque. Guarde su computadora portátil (hasta 15 pulgadas) en la funda acolchada, su uso diario",
+        "category": "Ropa de hombre",
         "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
         "rating": {
         "rate": 3.9,
@@ -15,9 +27,9 @@ const products = [
         {
         "id": 2,
         "title": "Mens Casual Premium Slim Fit T-Shirts ",
-        "price": 22.3,
-        "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-        "category": "men's clothing",
+        "price": 22,
+        "description": "Estilo ajustado, manga larga raglán en contraste, tapeta henley de tres botones, tejido ligero y suave para un uso cómodo y transpirable. Y camisas con costuras sólidas y cuello redondo hechas para mayor durabilidad y un excelente ajuste para ropa de moda informal y fanáticos acérrimos del béisbol. El escote redondo estilo Henley incluye una tapeta de tres botones.",
+        "category": "Ropa de hombre",
         "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
         "rating": {
         "rate": 4.1,
@@ -25,23 +37,24 @@ const products = [
         }
         },
         {
-        "id": 3,
-        "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
-        "price": 695,
-        "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-        "category": "jewelery",
-        "image": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
-        "rating": {
-        "rate": 4.6,
-        "count": 400
-        }
-        },
+          "id": 5,
+          "title": "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+          "price": 64,
+          "description": "Compatibilidad con USB 3.0 y USB 2.0 Transferencias de datos rápidas Mejora el rendimiento de la PC Alta capacidad; Compatibilidad NTFS formateado para Windows 10, Windows 8.1, Windows 7; Es posible que sea necesario volver a formatear para otros sistemas operativos; La compatibilidad puede variar según la configuración del hardware y el sistema operativo del usuario.",
+          "category": "Electronica",
+          "image": "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+          "rating": {
+          "rate": 3.3,
+          "count": 203
+          }
+          },
+
         {
         "id": 4,
         "title": "Solid Gold Petite Micropave ",
-        "price": 168,
-        "description": "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
-        "category": "jewelery",
+        "price": 170,
+        "description": "Satisfacción garantizada. Devuelva o cambie cualquier pedido dentro de los 30 días. Diseñado y vendido por Hafeez Center en los Estados Unidos.",
+        "category": "Joyeria",
         "image": "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
         "rating": {
         "rate": 3.9,
@@ -49,23 +62,23 @@ const products = [
         }
         },
         {
-        "id": 5,
-        "title": "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
-        "price": 64,
-        "description": "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
-        "category": "electronics",
-        "image": "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
-        "rating": {
-        "rate": 3.3,
-        "count": 203
-        }
-        },
+          "id": 8,
+          "title": "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+          "price": 30,
+          "description": "100 % POLIURETANO (cubierta) 100 % POLIÉSTER (forro) 75 % POLIÉSTER 25 % ALGODÓN (SUÉTER), Material de piel sintética para estilo y comodidad / 2 bolsillos delanteros, Chaqueta de piel sintética estilo mezclilla con capucha 2 por uno, Detalle de botones en cintura / Detalle de costuras en los costados, LAVAR SOLAMENTE A MANO / NO USAR LEJÍA / SECAR AL tendedero / NO PLANCHAR",
+          "category": "Ropa de mujer",
+          "image": "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
+          "rating": {
+          "rate": 2.9,
+          "count": 340
+          }
+          },
         {
         "id": 6,
         "title": "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
-        "price": 109,
-        "description": "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
-        "category": "electronics",
+        "price": 100,
+        "description": "Actualización sencilla para un inicio, apagado, carga de aplicaciones y respuesta más rápidos (en comparación con el disco duro SATA de 2,5” de 5400 RPM; basado en especificaciones publicadas y pruebas de evaluación comparativas internas utilizando puntajes PCMark vantage) Aumenta el rendimiento de escritura en ráfaga, lo que lo hace ideal para cargas de trabajo típicas de PC El equilibrio perfecto entre rendimiento y confiabilidad Velocidades de lectura/escritura de hasta 535 MB/s/450 MB/s (según pruebas internas; el rendimiento puede variar según la capacidad de la unidad, el dispositivo host, el sistema operativo y la aplicación).",
+        "category": "Electronica",
         "image": "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
         "rating": {
         "rate": 2.9,
@@ -75,27 +88,16 @@ const products = [
         {
         "id": 7,
         "title": "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
-        "price": 56.99,
-        "description": "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
-        "category": "women's clothing",
+        "price": 57,
+        "description": "Las chaquetas son de tamaño estándar de EE. UU., elija la talla que usa habitualmente. Material: 100% poliéster; Tejido del forro desmontable: forro polar cálido. Forro funcional desmontable: agradable al tacto, ligero y cálido. Chaqueta con forro de cuello alto que te mantiene abrigado en climas fríos. Bolsillos con cremallera: 2 bolsillos para las manos con cremallera, 2 bolsillos con cremallera en el pecho (suficiente para guardar tarjetas o llaves) y 1 bolsillo oculto en el interior. Los bolsillos para las manos con cremallera y el bolsillo oculto mantienen sus cosas seguras. Diseño humanizado: capucha ajustable y desmontable y puño ajustable para evitar el viento y el agua, para un ajuste cómodo. El diseño desmontable 3 en 1 brinda más comodidad, puede separar el abrigo y el interior según sea necesario, o usarlos juntos. Es adecuado para diferentes estaciones y le ayuda a adaptarse a diferentes climas.",
+        "category": "Ropa de mujer",
         "image": "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
         "rating": {
         "rate": 2.6,
         "count": 235
         }
         },
-        {
-        "id": 8,
-        "title": "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
-        "price": 29.95,
-        "description": "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
-        "category": "women's clothing",
-        "image": "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
-        "rating": {
-        "rate": 2.9,
-        "count": 340
-        }
-        },
+    
   ];
   
 // Obtener todas las categorías únicas de los productos
